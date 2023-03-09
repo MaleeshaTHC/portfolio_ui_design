82b6a05d2d95e54ebb10a4c027500c5b793870f2
MaleeshaTHC <96755@fhss.sjp.ac.lk>
2023-03-09 14:52:27 +0530

    update folder structure and start UI dev

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
-import { BrowserRouter } from "react-router-dom";
+import Gallery from './components/Gallery';
 
-import {
-  About,
-  Contact,
-  Footer,
-  Hero,
-  Navbar,
-  Content,
-  Works,
-} from "./components";
-
-const App = () => {
+export default function App() {
   return (
-    <BrowserRouter>
-      <div className="relative z-0 bg-primary">
-        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Content />
-        <Works />
-        <Works />
-        <div className="relative z-0">
-          <Contact />
-          <Footer />
-        </div>
-      </div>
-    </BrowserRouter>
+    <Gallery />
   );
-};
-
-export default App;
+}
